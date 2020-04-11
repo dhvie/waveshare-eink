5a746423454d71da7d8e6d165c109dbddf73804b
dhvie <dhvie85@gmail.com>
2020-04-11 19:07:57 +0000

    adding roboto

--- waveshare_eink/server/start.py
+++ waveshare_eink/server/start.py
@@ -1,10 +1,30 @@
 from flask import Flask
 from flask import render_template
+import jinja2 as j2
+import requests
+import argparse
 
-app = Flask("ws-eink")
+class OpenWeatherAPI():
 
-@app.route('/<page>', methods=['GET'])
-def pages(page=None):
-    if page is None:
-        page = 'main'
-    return render_template(f'{page}.html')
+    url = j2.Template('https://api.openweathermap.org/data/2.5/onecall?lat={{lat}}&lon={{lon}}&appid={{api_key}}')
+
+def main(args):
+    app = Flask("ws-eink")
+
+    @app.route('/<page>', methods=['GET'])
+    def pages(page=None):
+        if page is None:
+            page = 'main'
+        return render_template(f'{page}.html')
+
+
+    @app.route('/weather', methods=['GET'])
+    def weather():
+        pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--own", type=str)
+    args = parser.parse_args()
+    main(args)
