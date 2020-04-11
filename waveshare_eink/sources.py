cb57a9b42cd2baf767ff4a27f9d38599dc01c49f
dhvie <dhvie85@gmail.com>
2020-04-11 19:58:07 +0000

    roboto to weasy

--- waveshare_eink/sources.py
+++ waveshare_eink/sources.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from weasyprint import HTML, CSS
+from weasyprint.fonts import FontConfiguration
 from pathlib import Path
 
 class Source(ABC):
@@ -16,6 +17,7 @@ class SourceHTML(Source):
 
     def refresh(self, display):
         css_path = Path(__file__).parent / 'style' / 'page.css'
+        font_config = FontConfiguration()   
         HTML(self.__url).write_png(self.__output_path, stylesheets=[
             CSS(string=f'''
                 @page {{ 
@@ -24,10 +26,18 @@ class SourceHTML(Source):
                     padding: 0cm
                 }}
 
+                @font-face {{
+                    font-family: 'Roboto';
+                    font-style: normal;
+                    font-weight: 400;
+                    src: local('Roboto'), local('Roboto-Regular'), url(https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxP.ttf) format('truetype');
+                }}
+
                 body {{
                     background-color: #FFFFFF;
+                    font-family: Roboto
                 }}
-                '''),
+                ''', font_config=font_config),
                 CSS(css_path)
         ])
 
