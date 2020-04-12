1256c0e38ac2a0026da2244678119c6a3c029903
dhvie <dhvie85@gmail.com>
2020-04-12 08:21:23 +0000

    add icon

--- waveshare_eink/server/start.py
+++ waveshare_eink/server/start.py
@@ -9,6 +9,7 @@ import os
 class OpenWeatherAPI():
 
     url = j2.Template('https://api.openweathermap.org/data/2.5/onecall?lat={{lat}}&lon={{lon}}&appid={{api_key}}&units={{units}}')
+    icon_url = j2.Template('http://openweathermap.org/img/wn/{{icon}}@2x.png')
 
     def __init__(self, api_key):
         self.__api_key = api_key
@@ -16,6 +17,8 @@ class OpenWeatherAPI():
     def call(self, lon, lat, units='imperial'):
         return json.loads(requests.get(OpenWeatherAPI.url.render(api_key=self.__api_key, lon=lon, lat=lat, units=units)).text)
 
+    def get_icon_url(icon):
+        return OpenWeatherAPI.icon_url.render(icon=icon)
 
 args = {
     'own': os.environ['OWN_API'],
@@ -30,11 +33,13 @@ own_api = OpenWeatherAPI(args['own'])
 def get_weather():
     return own_api.call(args['lon'], args['lat'])
 
+@app.route('/', methods=['GET'])
 @app.route('/<page>', methods=['GET'])
 def pages(page=None):
     if page is None:
         page = 'main'
     weather = get_weather()
-    return render_template(f'{page}.html', weather=weather)
+    weather_icon = own_api.get_icon_url(weather['current']['weather']['icon'])
+    return render_template(f'{page}.html', weather=weather, weather_icon=weather_icon)
 
 
